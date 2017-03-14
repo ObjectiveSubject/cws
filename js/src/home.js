b1f7c47f197bedea9be829db04269108cbefaaf9
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-03-14 14:10:52 +0700

    remove setHeight JS in favor of css vh units

--- js/src/home.js
+++ js/src/home.js
@@ -8,10 +8,8 @@
 		height = $window.height();
 
 	if ( $('body').hasClass('home') ) {
-		setHeight();
 		
 		$window
-			.on('resize', setHeight)
 			.on('scroll', function(e){
 				var area = {
 						top: $window.scrollTop(),
@@ -39,16 +37,6 @@
 			var target = $(this).attr('href');
 			$('html,body').animate({scrollTop: $(target).offset().top + 'px'});
 		});
-	}
-
-	function setHeight() {
-		height = $window.height();
-		if ( window.outerWidth >= 768 ){
-			$('.home-section .outer-container').css('height', (height - 52) + 'px');
-		} else {
-			$('.home-section .outer-container').css('height', 'auto');
-			$('.intro-section .outer-container').css('height', (height - 42) + 'px');
-		}
 	}
 
 })(jQuery, window);
